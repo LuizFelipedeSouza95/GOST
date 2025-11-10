cb3f9e0dc44ea6272cf784b3d46f67341c537b4e
LuizSouza1995 <luizfelipedesouza94@gmail.com>
2025-11-10 11:10:55 -0300

    ajustando rotas do bd

--- api/auth/google.ts
+++ api/auth/google.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import { OAuth2Client } from 'google-auth-library';
 import { getEm } from '../_utils/orm.js';
-import { Usuario } from '../../server/entities/usuarios.entity.js';
+import { Usuario } from '../../server/entities/usuarios.entity';
 
 export default async function handler(req: any, res: any) {
 	// CORS headers (dev/prod)
